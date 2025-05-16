4b5f57652b453cf6e6f9feb5ff7b8be9c4757d87
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-16 10:34:18 +0200

    Added static method to calculate final amount

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
 package main.java;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Loaner");
+    public static void main(String[] args) throws ParseException    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to LLBank , you can get loan at anytime ]");
+
+        System.out.println(getLoan(10 , 5 , 2));
+
     }
+
+
+
+  static  int getLoan(int p , int n , int r) throws ParseException {
+        NumberFormat format = NumberFormat.getNumberInstance(Locale.UK);
+        int amount = p *(r * n);
+        String finalAmount= NumberFormat.getInstance().format(amount);
+
+
+        return (int ) format.parse(finalAmount);
+
+
+  }
 }
